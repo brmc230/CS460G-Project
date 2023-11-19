27ddd87ddfc7fc3cef348d0c11d0239ee42d6ac4
Brooke McWilliams <brmc230@gmail.com>
2023-11-19 15:54:45 -0500

    Added file header

---
+++ Audio_Features.py
@@ -0,0 +1,13 @@
+#################################################################################################################################
+# File:                 Audio_Features
+# Authors:              Brooke McWilliams
+# Date Created:         11/19/2023
+# Date Last Modified:   
+# Description:          Strip features out of audio files for our speech emotion recognition 
+# Misc:                 . . .
+#################################################################################################################################
+
+import os
+import librosa
+import matplotlib.pyplot as plt
+
