e39e1c0ce8c7e2d8fc6085d0b1cd89ea40cfd39b
Brooke McWilliams <brmc230@gmail.com>
2023-11-27 13:24:54 -0500

    Update

--- Audio_Features.py
+++ Audio_Features.py
@@ -10,3 +10,23 @@
 import os
 import librosa
 import matplotlib.pyplot as plt
+import pandas as pd
+
+path = "/Crema Dataset"
+
+emotionLabels = {"SAD": 0, "ANG": 1, "DIS": 2, "FEA": 3, "HAP": 4, "NEU": 5}
+
+def extract_features(file_path):
+    data, sampleRate = librosa.load(file_path)
+    data_features = librosa.feature.mfcc(y=data, sr=sampleRate, n_mfcc=20)
+    return data_features.flatten()
+
+wav_data = []
+for file in os.listdir(path):
+    file_path = os.path.join(path, file)
+    features = extract_features(file_path)
+    wav_data.append([file] + features.tolist())
+
+columns = ['filename'] + [f'feature_{i}' for i in range(len(wav_data[0]) - 1)]
+
+wave_dataframe = pd.DataFrame(wav_data, columns=columns)
