7574f853ca19e919eadae3354b86f9a6c6256580
jabi236 <123780845+jabi236@users.noreply.github.com>
2023-12-02 02:36:10 -0500

    now using data from feature extraction

--- CNN_Model.py
+++ CNN_Model.py
@@ -7,74 +7,88 @@
 # Misc:                 . . .
 #################################################################################################################################
 
+#from Audio_Features import *
+import pandas as pd
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Conv1D, Activation, BatchNormalization, Dropout, MaxPooling1D, Flatten, Dense
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 import matplotlib.pyplot as plt
 
 #################################################################################################################################
-def eval_model(X_train, y_train, X_test, y_test):
+def eval_model(X_train, y_train, X_test, y_test, num_feat):
     model = Sequential()
     model.add(Conv1D(filters=256, kernel_size=5, padding='same', input_shape=(X_train.shape[1],1)))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
     model.add(MaxPooling1D(pool_size=2))
+    model.add(Dropout(0.5))
 
     model.add(Conv1D(filters=256, kernel_size=5, padding='same'))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
     model.add(MaxPooling1D(pool_size=2))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
 
     model.add(Conv1D(filters=128, kernel_size=5, padding='same'))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
     model.add(MaxPooling1D(pool_size=2))
+    model.add(Dropout(0.5))
 
     model.add(Conv1D(filters=64, kernel_size=5, padding='same'))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
     model.add(MaxPooling1D(pool_size=2))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
 
     model.add(Conv1D(filters=32, kernel_size=5, padding='same'))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
     model.add(MaxPooling1D(pool_size=2))
+    model.add(Dropout(0.5))
 
     model.add(Flatten())
 
     model.add(Dense(60))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
 
-    model.add(Dense(6))
+    model.add(Dense(num_feat))
     model.add(Activation('softmax'))
     model.add(BatchNormalization())
 
     model.summary()
 
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.fit(X_train, y_train, epochs=20, batch_size=16, validation_data=(X_test, y_test))
     accuracy = model.evaluate(X_test, y_test, verbose=1)
 
     return accuracy[1]
 #################################################################################################################################
 
-# dumby data
-X = np.random.rand(1000, 50)
-y = np.random.randint(0, 6, size=1000)
-encoder = OneHotEncoder(sparse=False)
-y_1h = encoder.fit_transform(y.reshape(-1, 1))
+# read in data
+wave_df = pd.read_excel('Features.xlsx', header = 0, sheet_name='MFCC Features')
+#wave_df = pd.read_excel('Features.xlsx', header = 0, sheet_name='RMS')
+#wave_df = pd.read_excel('Features.xlsx', header = 0, sheet_name='Zero Crossing Rate')
+print('Download Complete!')
 
-X_train, X_test, y_train, y_test = train_test_split(X, y_1h, test_size=0.33, random_state=42)
+# converted wave data
+X = wave_df.drop(['ID/File','Labels'], axis=1)
+y = wave_df['Labels']
 
-acc = eval_model(X_train, y_train, X_test, y_test)
+encoder = LabelEncoder()
+y_1 = encoder.fit_transform(y)
+
+num_feat = len(pd.unique(y_1))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y_1, test_size=0.33, random_state=42)
+
+acc = eval_model(X_train, y_train, X_test, y_test, num_feat)
 print(f"Model Accuracy: {acc:.3f}")
