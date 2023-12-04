0be5afe5cd360766384649f261d90a3fe1679713
Brooke McWilliams <brmc230@gmail.com>
2023-12-03 23:23:22 -0500

    Refined the normalization on the features

--- Audio_Features.py
+++ Audio_Features.py
@@ -2,8 +2,8 @@
 # File:                 Audio_Features
 # Authors:              Brooke McWilliams
 # Date Created:         11/19/2023
-# Date Last Modified:   11/27/2023
-# Description:          Strip features out of audio files for our speech emotion recognition 
+# Date Last Modified:   12/03/2023
+# Description:          Strip features out of audio files for our speech emotion recognition using the librosa library 
 # Misc:                 . . .
 #################################################################################################################################
 
@@ -23,12 +23,13 @@ def get_data(filepath):
 path = "./Crema Dataset/"
 excel_path = "./NewFeatures.xlsx"
 
-wav_data_mfcc = []
-wav_data_rms = []
-wav_data_zcr = []
+wav_data_mfcc_norm = []
+wav_data_rms_norm = []
+wav_data_zcr_norm = []
 labelList = []
 wav_data = []
 fileName = []
+sampleR = []
 
 for file in os.listdir(path):
     if file.endswith(".wav"):               
@@ -36,6 +37,7 @@ for file in os.listdir(path):
         # Get audio data from each data file
         record, sr = get_data(file_path)
         wav_data.append(record)
+        sampleR.append(sr)
 
         # Labels for each data file
         labelList.append(file.split('_')[2])
@@ -60,21 +62,16 @@ for signal in wav_data:
     padS = np.pad(signal, (0, padW), mode="constant")
     padded_data.append(padS)
 
-for audio in padded_data:
-    wav_data_mfcc.append((librosa.feature.mfcc(y=audio, n_mfcc=20)).flatten())
-    wav_data_rms.append((librosa.feature.rms(y=audio)).flatten())
-    wav_data_zcr.append((librosa.feature.zero_crossing_rate(y=audio)).flatten())
+# Extract and normalize data features
+for audio, rate in zip(padded_data, sampleR):
+    wav_data_mfcc = librosa.feature.mfcc(y=audio, sr=rate, htk=True, n_mfcc=13).flatten()
+    wav_data_mfcc_norm.append((wav_data_mfcc - np.mean(wav_data_mfcc)) / np.std(wav_data_mfcc))
 
-# Normalize data features
-wav_data_mfcc = np.array(wav_data_mfcc)
-wav_data_rms = np.array(wav_data_rms)
-wav_data_zcr = np.array(wav_data_zcr)
+    wav_data_rms = librosa.feature.rms(y=audio).flatten()
+    wav_data_rms_norm.append((wav_data_rms - np.mean(wav_data_rms)) / np.std(wav_data_rms))
 
-scale = StandardScaler()
-
-wav_data_mfcc_norm = scale.fit_transform(wav_data_mfcc)
-wav_data_rms_norm = scale.fit_transform(wav_data_rms)
-wav_data_zcr_norm = scale.fit_transform(wav_data_zcr)
+    wav_data_zcr = librosa.feature.zero_crossing_rate(y=audio).flatten()
+    wav_data_zcr_norm.append((wav_data_zcr - np.min(wav_data_zcr)) / (np.max(wav_data_zcr) - np.min(wav_data_zcr)))
 
 # Create dataframes to work with for each feature extraction method 
 # MFCC
@@ -92,15 +89,11 @@ wave_zcr_dataframe = pd.DataFrame(wav_data_zcr_norm)
 wave_zcr_dataframe.insert(0, "ID/File", fileName)
 wave_zcr_dataframe.insert(1, "Labels", labelList)
 
-
 # 3 Total sheets in the file for 3 different feature extraction methods
 with pd.ExcelWriter(excel_path, engine="xlsxwriter") as writer:
-    # Sheet has 7442 rows and 4322 columns
-    # There are 4320 features extracted
+
     wave_mfcc_dataframe.to_excel(writer, sheet_name="MFCC Features", index=False)
-    # Sheet has 7442 rows and 218 columns
-    # There are 216 features extracted 
+
     wave_rms_dataframe.to_excel(writer,sheet_name="RMS", index=False)
-    # Sheet has 7442 rows and 218 columns
-    # There are 216 features extracted 
+
     wave_zcr_dataframe.to_excel(writer, sheet_name="Zero Crossing Rate", index=False)
